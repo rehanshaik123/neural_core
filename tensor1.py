220818f2ee2de2e113f3bf70b1ec5cf1b5953734
rehanshaik123 <shaikrehan22463@gmail.com>
2026-06-21 16:25:27 +0530

    done practicing y=wx+b

--- tensor1.py
+++ tensor1.py
@@ -1,40 +1,42 @@
 from unittest import result
 
 import numpy as np
-# fake_image = np.array([0, 255, 128, 20])
-# # print(fake_image)
+fake_image = np.array([0, 255, 128, 20])
+# print(fake_image)
+# ---------------------------------------------------------------------------------
+# We tell numpy to rearrange the 4 numbers into 2 rows and 2 columns
+grid_image = fake_image.reshape(2, 2)
 
-# # We tell numpy to rearrange the 4 numbers into 2 rows and 2 columns
-# grid_image = fake_image.reshape(2, 2)
+print(grid_image)
+print(grid_image.shape)
 
-# print(grid_image)
-# print(grid_image.shape)
+ # ---------------------------------------------------------------------------------
 
- 
-
-# # A flat list of 8 pixels (enough for two 2x2 images)
-# fake_batch = np.array([0, 255, 128, 20, 255, 0, 50, 100])
+# A flat list of 8 pixels (enough for two 2x2 images)
+fake_batch = np.array([0, 255, 128, 20, 255, 0, 50, 100])
 
 # # CHALLENGE: Reshape this into a batch of 2 images, where each image is 2x2
 # # Hint: You need three numbers inside your reshape: (number_of_images, rows, columns)
-# batch_image = fake_batch.reshape(2, 2, 2)
+batch_image = fake_batch.reshape(2, 2, 2)
 
-# print("Our 3D Batch Layout:")
-# print(batch_image)
-# print("Shape:", batch_image.shape)
+print("Our 3D Batch Layout:")
+print(batch_image)
+print("Shape:", batch_image.shape)
  
 
-# fake_batch = np.array([0, 255, 128, 20, 255, 0, 50, 100])
-# batch_image = fake_batch.reshape(2, 2, 2)
+fake_batch = np.array([0, 255, 128, 20, 255, 0, 50, 100])
+batch_image = fake_batch.reshape(2, 2, 2)
+
+ # ---------------------------------------------------------------------------------
 
 # # Normalization step
 # # We divide by 255.0 (the decimal .0 forces Python to convert integers to floats)
-# normalized_batch = batch_image / 255.0
+normalized_batch = batch_image / 255.0
 
-# print("Normalized Decimal Batch:")
-# print(normalized_batch)
+print("Normalized Decimal Batch:")
+print(normalized_batch)
 
- 
+ # ---------------------------------------------------------------------------------
 # A single image with 3 input pixels
 X = np.array([0.5, 0.8, 0.1])
 print("Input Pixels (X):", X)
@@ -54,3 +56,16 @@ B = np.array([0.1, 0.2])  # Bias for Neuron 1 and Neuron 2
 
 result = np.dot(X, W) + B  # Matrix multiplication and adding 
 print("\nResult after applying weights and bias:", result)
+
+# # Input Pixels (X): [0.5 0.8 0.1]
+# Input Shape: (3,)
+
+# Weight Matrix (W):
+#  [[0.2 0.7]
+#  [0.5 0.1]
+#  [0.9 0.3]]
+# Weight Shape: (3, 2)
+
+# Result after applying weights and bias: [0.69 0.66]
+
+# ---------------------------------------------------------------------------------
